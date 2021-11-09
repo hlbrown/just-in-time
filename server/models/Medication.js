b780735c915c3db1a8acf9fda9aee20962ae458e
BVanBuren <bvanburenwx@gmail.com>
2021-11-08 19:08:09 -0500

    added the models for profile and medications

--- server/models/Medication.js
+++ server/models/Medication.js
@@ -3,32 +3,30 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const medicationSchema = new Schema({
-  // name: {
-  //   type: String,
-  //   required: true,
-  //   trim: true
-  // },
-  // description: {
-  //   type: String
-  // },
-  // image: {
-  //   type: String
-  // },
-  // price: {
-  //   type: Number,
-  //   required: true,
-  //   min: 0.99
-  // },
-  // quantity: {
-  //   type: Number,
-  //   min: 0,
-  //   default: 0
-  // },
-  // category: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'Category',
-  //   required: true
-  // }
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  brand: {
+    type: String,
+  },
+  prescription: {
+    type: Boolean,
+    required: true
+  },
+  OTC: {
+    type: Boolean,
+    required: true
+  }, 
+  dose: {
+    type: String,
+    required: true,
+    trim: true
+  }, 
+  interactions: {
+    type: String
+  }
 });
 
 const Medication = mongoose.model('Medication', medicationSchema);
