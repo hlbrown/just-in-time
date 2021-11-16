d6c9e74726a5876f2c6875570af7d951fe1bceb7
Hannah Brown <nagle.hannah1@gmail.com>
2021-11-15 21:44:08 -0500

    should be up to date

---
+++ client/src/pages/User.js
@@ -0,0 +1,10 @@
+import React, {Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Login } from 'utils/mutations';
+import Nav from 'components/Nav';
+
+
+
+export default function User() {
+    
+}
