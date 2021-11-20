a26a3ffbf5ee6dd8035fa0e9c308e8f643df0229
BVanBuren <bvanburenwx@gmail.com>
2021-11-20 00:20:54 -0500

    created new build

---
+++ client/build/precache-manifest.68cbf3a42d0bc024430ac4a478774ccc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3ae061d336ff42518ed104176108cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0feceb959979fa46bf6",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "af93e16fde3514fef6fa",
+    "url": "/static/css/main.8b263460.chunk.css"
+  },
+  {
+    "revision": "b0feceb959979fa46bf6",
+    "url": "/static/js/2.a3d3fee5.chunk.js"
+  },
+  {
+    "revision": "7c18fb661c08fee6393ddf94e75f2dd3",
+    "url": "/static/js/2.a3d3fee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af93e16fde3514fef6fa",
+    "url": "/static/js/main.7e441da1.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "80d44a0eec761d9274360f2acc489c81",
+    "url": "/static/media/familyIcon.80d44a0e.jpg"
+  },
+  {
+    "revision": "110608af07a4e48cc41bd0934ee15564",
+    "url": "/static/media/newestLogo.110608af.png"
+  },
+  {
+    "revision": "ca07c9b080e7b261e6890037f9036915",
+    "url": "/static/media/newestLogo2.ca07c9b0.png"
+  },
+  {
+    "revision": "44d91d4cecc78b73b08bf303418618c6",
+    "url": "/static/media/parentOrguradian.44d91d4c.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
