9cdc3095d1e9380b2d954363f91ac3936df5438b
Hannah Brown <nagle.hannah1@gmail.com>
2022-01-22 16:49:03 -0500

    took out the card background

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -62,9 +62,9 @@ function DashboardContent(props) {
       }}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={4}>
-          <Item>
+          
             <UserDisplay />
-          </Item>
+         
         </Grid>
         <Grid item xs={12} md={8}>
           <ProfileDisplay/>
